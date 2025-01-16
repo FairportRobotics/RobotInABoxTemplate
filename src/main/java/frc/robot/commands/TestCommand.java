85496fde4ccfe0d6309304ef499e4c3a95faade9
Nanson Chen <buyingsaucer2@gmail.com>
2025-01-16 18:45:52 -0500

    Resolving bad branch merging

--- src/main/java/frc/robot/commands/TestCommand.java
+++ src/main/java/frc/robot/commands/TestCommand.java
@@ -14,40 +14,34 @@ public class TestCommand extends Command {
 
   /**
    * Constructs a new instance of TestCommand.
+   * 
    * @param testSubsystem The testSubsystem used by this command
    */
   public TestCommand(TestSubsystem testSubsystem) {
     this.testSubsystem = testSubsystem;
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(new Subsystem[] {testSubsystem});
+    addRequirements(new Subsystem[] { testSubsystem });
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {testSubsystem.runMotor();}
+  public void execute() {
+    testSubsystem.runMotor();
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
-  }
-
-  /**
-   * Helper for the RobotContainer code.
-   * @param motorActivated is true when the motor should be active, false otherwise
-   * @return the command itself to be a valid method for RobotContainer parameters.
-   */
-  public Command setMotor(boolean motorActivated)
-  {
-    TestSubsystem.setMotor(motorActivated);
-    return this;
   }
 }
--- src/main/java/frc/robot/subsystems/TestSubsystem.java
+++ src/main/java/frc/robot/subsystems/TestSubsystem.java
@@ -1,4 +1,5 @@
 package frc.robot.subsystems;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -9,29 +10,24 @@ public class TestSubsystem extends SubsystemBase {
      * Stores the motor reference to the robot.
      */
     public final TalonSRX motor = new TalonSRX(Constants.MotorConstants.MOTOR_ID);
-    
+
     /**
      * Stores whether the robot should be active.
      */
     private boolean motorActivated;
 
     /**
-     * Constructor of the Test Subsystem.
-     * @param motorActivated is true when the motor should be on at that time, false otherwise.
-     */
-    private static boolean motorActivated = false;
-
-    /**
      * Constructs a new instance of TestSubsystem.
      */
-    public TestSubsystem()
-    {}
+    public TestSubsystem(boolean motorActivated) {
+        this.motorActivated = motorActivated;
+    }
 
     /**
      * Runs the motor depending on if the motor should be active.
      */
     public void runMotor() {
-        if(motorActivated)
+        if (motorActivated)
             motor.set(ControlMode.PercentOutput, .1);
         else
             motor.set(ControlMode.PercentOutput, 0);
@@ -39,10 +35,10 @@ public class TestSubsystem extends SubsystemBase {
 
     /**
      * A setter for motor activation.
+     * 
      * @param motorActivated is true if the motor should be active, false otherwise.
      */
-    public static void setMotor(boolean motorActivated)
-    {
-        TestSubsystem.motorActivated = motorActivated;
+    public void setMotor(boolean motorActivated) {
+        this.motorActivated = motorActivated;
     }
 }
